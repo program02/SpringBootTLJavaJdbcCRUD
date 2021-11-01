7645f2f501e63dd9b4a73c809de2df0e8ec1100d
shahidur24 <shahidur2424@gmail.com>
2021-11-01 18:34:23 +0600

    SB @Controller, @RestController, @ResponseBody, @PathVariable

--- src/main/java/com/exam/StudentController.java
+++ src/main/java/com/exam/StudentController.java
@@ -3,11 +3,16 @@ package com.exam;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+//@Controller  //Every-thing operation working with Controller
+@RestController
 public class StudentController {
 	@RequestMapping("/")
 	public ModelAndView home(ModelAndView m) {
@@ -26,6 +31,19 @@ public class StudentController {
 		return m;
 	}
 	
+	
+//	//even @ResponseBody this one no need if we use @RestController
+//	@RequestMapping("/all")
+//	public @ResponseBody List<Student> allstudents(ModelAndView m) {
+//		StudentDao dao = new StudentDao();
+//		List<Student> list = dao.allStudents();
+////		m.addObject("stList", list);
+////		m.setViewName("result.html");
+////		System.out.println(list.get(0).id);
+//		return list;
+//	}
+	
+	
 	@RequestMapping("/showbyid")
 	public ModelAndView showbyid(ModelAndView m,Student s) {
 		StudentDao dao = new StudentDao();
@@ -36,6 +54,23 @@ public class StudentController {
 		m.setViewName("result.html");
 		return m;
 	}
+	
+//	
+//	@ResponseBody()
+//	//@RequestMapping(value="/showbyid/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+//	@RequestMapping(value="/showbyid/{id}", produces = {MediaType.APPLICATION_JSON_VALUE})
+//	//@RequestMapping(value="/showbyid/{id}", produces = {MediaType.APPLICATION_XML_VALUE}) //need gson library.
+//	public Student showbyid(ModelAndView m, @PathVariable("id") int id) {
+//		StudentDao dao = new StudentDao();
+//		Student st = dao.showbyid(id);
+////		List<Student> list = new ArrayList<>();
+////		list.add(st);
+////		m.addObject("stList", list);
+////		m.setViewName("result.html");
+//		return st;
+//	}
+	
+	
 	@RequestMapping("/insert")
 	public ModelAndView insert(ModelAndView m,Student s) {
 		StudentDao dao = new StudentDao();
